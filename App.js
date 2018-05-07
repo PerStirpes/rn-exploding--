bd37bf7004ca9e28a1c2ca1b2dcc1c1f814f8ce3
Matt Barraza-Carl <mccarl@asu.edu>
2018-05-07 11:17:00 -0700

    add methods

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View
+} from "react-native";
+import Heart from "./heart";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: false,
+      scale: new Animated.Value(0),
+      animations: [
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0)
+      ]
+    };
+    this.triggerLike = this.triggerLike.bind(this);
+  }
+
+  triggerLike() {
+    this.setState({ liked: !this.state.like });
+
+    Animated.spring(this.state.scale, {
+      toValue: 2,
+      friction: 3
+    }).start(() => {
+      this.state.scale.setValue(0);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View>
+          <TouchableWithoutFeedback onPress={() => this.triggerLike}>
+            <Animated.View>
+              <Heart />Open up App.js to start working on your app!
+            </Animated.View>
+          </TouchableWithoutFeedback>
+        </View>
       </View>
     );
   }
@@ -16,8 +55,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
